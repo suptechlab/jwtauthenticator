aefbf21317e2e25330027ff34ccdcabd57dbfbe3
Mr. Zero <mr_z_ro@Mrs-MacBook-Air.local>
2024-09-03 15:32:03 -0400

    modified setup to specify installation requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='2.0.3',
     description='JSONWebToken Authenticator for JupyterHub',
     url='https://github.com/suptechlab/jwtauthenticator',
-    author='ppodolsky',
+    author='mr_z_ro',
     author_email='matt@digi.studio',
     license='Apache 2.0',
     packages=['jwtauthenticator'],
